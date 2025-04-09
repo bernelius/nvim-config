f94ea7437e3bbfaeb147f28dae892baf256e556a
Nathrak <bob@bernelius.com>
2025-04-09 20:41:14 +0200

    fzf cmake

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,27 +1,38 @@
 return {
 	{
-		'nvim-telescope/telescope.nvim', tag = '-1.1.8',
-		dependencies = { 'nvim-lua/plenary.nvim' },
-		config = function()
-			local builtin = require('telescope.builtin')
-			vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-			vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-			vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
-			vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
-		end
+		"nvim-telescope/telescope-fzf-native.nvim",
+		build = "make",
 	},
 	{
-		'nvim-telescope/telescope-ui-select.nvim',
+		"nvim-telescope/telescope.nvim",
+		tag = "-1.1.8",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			local builtin = require("telescope.builtin")
+			vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
+			vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
+			vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
+			vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
+		end,
+	},
+	{
+		"nvim-telescope/telescope-ui-select.nvim",
 		config = function()
 			require("telescope").setup({
 				extensions = {
+					fzf = {
+					fuzzy = true,
+					override_generic_sorter = true,
+					override_file_sorter = true,
+					case_mode = "smart_case",
+					},
 					["ui-select"] = {
-						require("telescope.themes").get_dropdown {
-						}
-					}
-				}
+						require("telescope.themes").get_dropdown({}),
+					},
+				},
 			})
+			require("telescope").load_extension("fzf")
 			require("telescope").load_extension("ui-select")
-		end
-	}
+		end,
+	},
 }
