fd8681b3ec29d1da49b4e73720f2d117c8d6f1b1
Nathrak <bob@bernelius.com>
2025-06-09 22:30:25 +0200

    disable ruff hover in lsp-config

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -17,16 +17,20 @@ return {
 		"neovim/nvim-lspconfig",
 		config = function()
 			local lspconfig = require("lspconfig")
-			local capabilities = require('cmp_nvim_lsp').default_capabilities()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 			lspconfig.lua_ls.setup({ capabilities = capabilities })
 			lspconfig.ts_ls.setup({ capabilities = capabilities })
 			lspconfig.clangd.setup({ capabilities = capabilities })
 			lspconfig.html.setup({ capabilities = capabilities })
-			lspconfig.jinja_lsp.setup({ capabilities = capabilities, lang={ 'python' }, filetypes={ 'html', 'htmldjango' }})
+			lspconfig.jinja_lsp.setup({
+				capabilities = capabilities,
+				lang = { "python" },
+				filetypes = { "html", "htmldjango" },
+			})
 			lspconfig.cssls.setup({ capabilities = capabilities })
 			lspconfig.ruff.setup({ capabilities = capabilities })
-			lspconfig.pyright.setup {
+			lspconfig.pyright.setup({
 				settings = {
 					pyright = {
 						-- Using Ruff's import organizer
@@ -39,11 +43,25 @@ return {
 						},
 					},
 				},
-			}
+			})
 
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set({ "n", "c" }, "<leader>ca", vim.lsp.buf.code_action, {})
+			vim.api.nvim_create_autocmd("LspAttach", {
+				group = vim.api.nvim_create_augroup("lsp_attach_disable_ruff_hover", { clear = true }),
+				callback = function(args)
+					local client = vim.lsp.get_client_by_id(args.data.client_id)
+					if client == nil then
+						return
+					end
+					if client.name == "ruff" then
+						-- Disable hover in favor of Pyright
+						client.server_capabilities.hoverProvider = false
+					end
+				end,
+				desc = "LSP: Disable hover capability from Ruff",
+			})
 		end,
 	},
 }
